4c599b0b52b05b3f0df842099ab62343b6df2eae
ToniEdwards <90237519+ToniEdwards@users.noreply.github.com>
2022-03-09 03:02:49 -0600

    Updated data function work in progress

--- sql_data.py
+++ sql_data.py
@@ -1,15 +1,58 @@
-from sqlalchemy import create_engine, inspect
+#import dependencies
+from sqlalchemy import create_engine
+import pandas as pd
 import json
 
+#begin our function
 def query():
-    data_dict = {}
+    #make db connection
     database_path ="resources/survivalprediction.sqllite"
     engine = create_engine(f"sqlite:///{database_path}")
-    gender = engine.execute("Select distinct gender from prediction").fetchall()
-    income = engine.execute("Select distinct Median_Household_Income from prediction").fetchall()
-    type = engine.execute("Select distinct Cancer_Type from prediction").fetchall()
-    stage = engine.execute("Select distinct Cancer_Stage from prediction").fetchall()
-    site = engine.execute("Select distinct Cancer_Site from prediction").fetchall()
-    race = engine.execute("Select distinct race from prediction").fetchall()
-    data_dict = {"site":site, "type":type, "stage":stage, "gender":gender, "income":income, "race":race}
-    return data_dict
+    #query db
+    query = """Select distinct * from prediction"""
+    cursor = engine.execute(query)
+    all_data = cursor.all()
+    #create df from result
+    all_df = pd.DataFrame(all_data)
+    all_df.columns = all_data[0].keys()
+    clean_all_df = all_df.drop(["Age", "Primary_Key"], axis=1)
+    #create iterative objects with only unique values
+    Cancer_Stage = clean_all_df["Cancer_Stage"].drop_duplicates()
+    Gender = clean_all_df["Gender"].drop_duplicates()
+    Cancer_Site = clean_all_df["Cancer_Site"].drop_duplicates()
+    Race = clean_all_df["Race"].drop_duplicates()
+    Median_Household_Income = clean_all_df["Median_Household_Income"].drop_duplicates()
+    Cancer_Type = clean_all_df["Cancer_Type"].drop_duplicates()
+    #create lists to store unique values
+    site=[]
+    ctype=[]
+    stage=[]
+    gender=[]
+    race=[]
+    income=[]
+    #iterate through the objects
+    for row in Cancer_Site:
+        if row not in site:
+            site.append(row)
+    for row in Cancer_Stage:
+        if row not in stage:
+            stage.append(row)
+    for row in Cancer_Type:
+        if row not in ctype:
+            ctype.append(row)
+    for row in Gender:
+        if row not in gender:
+            gender.append(row)
+    for row in Race:
+        if row not in race:
+            race.append(row)
+    for row in Median_Household_Income:
+        if row not in income:
+            income.append(row)
+    #create a dictionary to hold our lists
+    clean_dict = {"stage":stage, "gender":gender, "site":site, "ethnicity":race, "income":income, "type":ctype}
+    #jsonify the dictionary
+    sql_json = json.dumps(clean_dict)
+    
+    #end our function
+    return sql_json
